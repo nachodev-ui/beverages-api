ea39f69422b82d8906f2291e7a2d06085bc105c6
nachodev-ui <ig.cisternas@duocuc.cl>
2023-07-25 05:25:39 -0400

    feat: Beverages custom hook

---
+++ src/hooks/useBeverages.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import BeveragesContext from '../context/BeveragesProvider'
+
+const useBeverages = () => {
+
+    return useContext(BeveragesContext)
+}
+
+export default useBeverages 
